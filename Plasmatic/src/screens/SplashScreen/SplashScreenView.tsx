5d9f04fc0aa7ba6e609d869514e6ddba22d94ba8
Alex Korzh <alexkorzh7@gmail.com>
2021-07-05 00:46:30 +0200

    Add splash screen placeholder

---
+++ Plasmatic/src/screens/SplashScreen/SplashScreenView.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+const SplashScreenView = () => {
+  return (
+    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Splash!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SplashScreenView;
