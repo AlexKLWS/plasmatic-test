87175da06582d4124249b10ffb526ba7e9aa0d1c
Alex Korzh <alexkorzh7@gmail.com>
2021-07-05 00:47:21 +0200

    Add login screen placeholder

---
+++ Plasmatic/src/screens/LoginScreen/LoginScreenController.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoginScreenView from './LoginScreenView';
+
+const LoginScreenController = () => {
+  return <LoginScreenView />;
+};
+
+export default LoginScreenController;
---
+++ Plasmatic/src/screens/LoginScreen/LoginScreenView.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+const LoginScreenView = () => {
+  return (
+    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Login!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreenView;
