90c1f3fd6374cdc9a83e566de024ddc805173c45
Alex Korzh <alexkorzh7@gmail.com>
2021-07-05 15:27:01 +0200

    Add colors consts to shared styles

---
+++ Plasmatic/src/shared/styles/colors.ts
@@ -0,0 +1,14 @@
+const colors = {
+  primary: {
+    blue: '#2c98f0',
+    red: '#E32C4D',
+    black: '#000000',
+  },
+  secondary: {
+    dark: '#5C6170',
+    light: '#9AA0AC',
+    white: '#FFFFFF',
+  },
+};
+
+export default colors;
