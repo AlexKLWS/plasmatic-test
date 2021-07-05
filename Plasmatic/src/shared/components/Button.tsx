095dab59c642c72a099d68c484f07bef953faaa5
Alex Korzh <alexkorzh7@gmail.com>
2021-07-05 23:04:33 +0200

    Add button shared component

---
+++ Plasmatic/src/shared/components/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { scale } from '~/helpers/scale';
+import styleSystem from '~/shared/styles';
+
+type Props = {
+  text?: string;
+  onPress: () => void;
+};
+
+const Button: React.FC<Props> = props => {
+  return (
+    <TouchableOpacity activeOpacity={0.7} style={styles.container} onPress={props.onPress}>
+      <View>
+        <Text style={styles.text}>{props.text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: styleSystem.colors.primary.blue,
+    paddingVertical: scale(16),
+    borderRadius: 32,
+    width: '100%',
+    alignItems: 'center',
+  },
+  text: {
+    ...styleSystem.typography.button,
+  },
+});
+
+export default Button;
