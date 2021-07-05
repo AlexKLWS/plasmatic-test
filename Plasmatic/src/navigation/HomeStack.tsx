e135b8262f907ed2157995745ee1c0f0bd3ada62
Alex Korzh <alexkorzh7@gmail.com>
2021-07-06 01:06:22 +0200

    Use custom tab bar on home stack

--- Plasmatic/src/navigation/HomeStack.tsx
+++ Plasmatic/src/navigation/HomeStack.tsx
@@ -10,32 +10,15 @@ import HomeIcon from '~/assets/icons/tabBar/HomeIcon';
 import ServicesIcon from '~/assets/icons/tabBar/ServicesIcon';
 import PartnersIcon from '~/assets/icons/tabBar/PartnersIcon';
 import ActivityIcon from '~/assets/icons/tabBar/ActivityIcon';
+import { View } from 'react-native';
+import { scale } from '~/helpers/scale';
+import HomeTabBar from '~/components/tabBar/homeTabBar';
 
 const Tab = createBottomTabNavigator();
 
 const HomeStack = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          switch (route.name) {
-            case 'Home':
-              return <HomeIcon color={color} />;
-            case 'Services':
-              return <ServicesIcon color={color} />;
-            case 'Partners':
-              return <PartnersIcon color={color} />;
-            case 'Activity':
-              return <ActivityIcon color={color} />;
-            default:
-              break;
-          }
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: styleSystem.colors.primary.blue,
-        inactiveTintColor: styleSystem.colors.secondary.light,
-      }}>
+    <Tab.Navigator tabBar={props => <HomeTabBar {...props} />}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Services" component={ServicesScreen} />
       <Tab.Screen name="Partners" component={PartnersScreen} />
