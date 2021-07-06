7b8197aa8de3f647a1efa6a787eed808fa278c91
Alex Korzh <alexkorzh7@gmail.com>
2021-07-06 23:50:10 +0200

    Add icons and events view to the home screen view

--- Plasmatic/src/screens/HomeScreen/HomeScreenView.tsx
+++ Plasmatic/src/screens/HomeScreen/HomeScreenView.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Text, View } from 'react-native';
+import CalendarIcon from '~/assets/icons/home/CalendarIcon';
+import PeopleIcon from '~/assets/icons/home/PeopleIcon';
+import PostsIcon from '~/assets/icons/home/PostsIcon';
+import EventsView from '~/components/eventsView/EventsView';
 
-import {SafeAreaView, Text} from 'react-native';
+import HomeScreenHeader from '~/components/homeScreenHeader/HomeScreenHeader';
+import TabView from '~/shared/components/TabView';
 
 const HomeScreenView = () => {
+  const homeViews = [
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home1</Text>
+    </View>,
+    <EventsView />,
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home3</Text>
+    </View>,
+  ];
+
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home!</Text>
-    </SafeAreaView>
+    <View style={{ flex: 1 }}>
+      <HomeScreenHeader />
+      <TabView
+        defaultTabIndex={1}
+        tabsContent={homeViews}
+        tabsIds={['Posts', 'Events', 'Connect']}
+        tabIcons={[
+          (props: any) => <PostsIcon {...props} />,
+          (props: any) => <CalendarIcon {...props} />,
+          (props: any) => <PeopleIcon {...props} />,
+        ]}
+      />
+    </View>
   );
 };
 
