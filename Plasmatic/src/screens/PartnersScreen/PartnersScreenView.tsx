d147cc38d24f0e07368b8a37a033eced7677e311
Alex Korzh <alexkorzh7@gmail.com>
2021-07-07 01:59:31 +0200

    Setup partners screen view layout

--- Plasmatic/src/screens/PartnersScreen/PartnersScreenView.tsx
+++ Plasmatic/src/screens/PartnersScreen/PartnersScreenView.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
 
-import {SafeAreaView, Text} from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import PartnerOfferItem from '~/components/partnerOfferItem/PartnerOfferItem';
+import PartnersScreenHeader from '~/components/partnersScreenHeader/PartnersScreenHeader';
+import SmallEventItem from '~/components/smallEventItem/SmallEventItem';
+import { scale } from '~/helpers/scale';
+import styleSystem from '~/shared/styles';
 
 const PartnersScreenView = () => {
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Dashboard!</Text>
-    </SafeAreaView>
+    <View style={{ flex: 1 }}>
+      <PartnersScreenHeader searchPartners={() => undefined} />
+      <ScrollView>
+        <View style={styles.offersSectionContainer}>
+          <FlatList
+            nestedScrollEnabled
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={[1, 2, 3]}
+            keyExtractor={i => `${i}`}
+            renderItem={() => <SmallEventItem />}
+            contentContainerStyle={{ flexGrow: 1, paddingHorizontal: scale(16) }}
+          />
+          <View style={styles.offersList}>
+            <Text style={[styleSystem.typography.labelBig, { paddingBottom: scale(16) }]}>Top Offers</Text>
+            <PartnerOfferItem />
+            <PartnerOfferItem />
+          </View>
+        </View>
+        <Text>Partners!</Text>
+      </ScrollView>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  offersSectionContainer: {
+    marginVertical: scale(16),
+    backgroundColor: styleSystem.colors.secondary.white,
+    paddingVertical: scale(16),
+  },
+  offersList: {
+    paddingTop: scale(16),
+    paddingHorizontal: scale(24),
+  },
+});
 
 export default PartnersScreenView;
