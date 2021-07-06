25e4deabf16716f9dac31715f5c13d35b5217890
Alex Korzh <alexkorzh7@gmail.com>
2021-07-06 02:11:35 +0200

    Change feed screen to activity

--- Plasmatic/src/screens/ActivityScreen/ActivityScreenView.tsx
+++ Plasmatic/src/screens/ActivityScreen/ActivityScreenView.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import {SafeAreaView, Text} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 const ActivityScreenView = () => {
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Feed!</Text>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Activity!</Text>
     </SafeAreaView>
   );
 };
