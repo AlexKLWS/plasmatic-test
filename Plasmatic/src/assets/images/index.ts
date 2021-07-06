3e1f1bcf663024080fb3a048b71b69696acefde5
Alex Korzh <alexkorzh7@gmail.com>
2021-07-06 04:01:24 +0200

    Add events images

--- Plasmatic/src/assets/images/index.ts
+++ Plasmatic/src/assets/images/index.ts
@@ -2,4 +2,10 @@ export default {
   portrait1: require('./portrait/1.webp'),
   portrait2: require('./portrait/2.webp'),
   portrait3: require('./portrait/3.webp'),
+  events1: require('./events/1.webp'),
+  events2: require('./events/2.webp'),
+  events3: require('./events/3.webp'),
+  events4: require('./events/4.webp'),
+  events5: require('./events/5.webp'),
+  events6: require('./events/6.webp'),
 };
