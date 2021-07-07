a26effa2d6f46759a64b431c567b91203d31744f
Alex Korzh <alexkorzh7@gmail.com>
2021-07-08 01:48:49 +0200

    Add blue background to splash screen

--- Plasmatic/src/screens/SplashScreen/SplashScreenView.tsx
+++ Plasmatic/src/screens/SplashScreen/SplashScreenView.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-import {SafeAreaView, Text} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
+import styleSystem from '~/shared/styles';
 
 const SplashScreenView = () => {
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: styleSystem.colors.primary.blue,
+      }}>
       <Text>Splash!</Text>
     </SafeAreaView>
   );
