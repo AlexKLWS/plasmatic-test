2efdbccc02f6d66b4dc6df7e8971da7ea87cd71b
Alex Korzh <alexkorzh7@gmail.com>
2021-07-07 23:40:49 +0200

    Add sign in method tests

--- Plasmatic/src/services/session.spec.ts
+++ Plasmatic/src/services/session.spec.ts
@@ -1,5 +1,6 @@
 import { Container } from 'inversify';
 import mockAuth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 import { ISessionService, SessionServiceId, SessionService } from '~/services/session';
 import { UserAuthStatus } from '~/types/userAuthStatus';
@@ -53,5 +54,37 @@ describe('userStatus', () => {
     instance.userStatus.subscribe(subscriptionCallback);
 
     expect(subscriptionCallback).toBeCalledWith(UserAuthStatus.AUTHORIZED);
+  });
+});
+
+describe('signIn', () => {
+  it('Should successfully sign in', async () => {
+    // @ts-expect-error
+    mockAuth().onAuthStateChanged.mockImplementationOnce((callback: (user: FirebaseAuthTypes.User | null) => void) => {
+      callback(null);
+    });
+    // @ts-expect-error
+    mockAuth.GoogleAuthProvider.credential.mockImplementationOnce((token: string) => {
+      expect(token).toEqual('GOOGLE_AUTH_TOKEN');
+      return { auth: true };
+    });
+    // @ts-expect-error
+    mockAuth().signInWithCredential.mockImplementationOnce((status: any) => {
+      expect(status.auth).toEqual(true);
+      return { user: mockUser };
+    });
+
+    const myContainer = new Container();
+    myContainer.bind<ISessionService>(SessionServiceId).to(SessionService);
+
+    const instance = myContainer.get<ISessionService>(SessionServiceId);
+
+    const subscriptionCallback = jest.fn();
+    instance.userStatus.subscribe(subscriptionCallback);
+    expect(subscriptionCallback).toBeCalledWith(UserAuthStatus.UNAUTHORIZED);
+
+    const [firebaseUserCredential, error] = await instance.signIn();
+    expect(firebaseUserCredential?.user).toEqual(mockUser);
+    expect(error).toBeFalsy();
   });
 });
