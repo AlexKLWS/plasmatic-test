ec9cf17f5ac4e7b9bde3e4712a5f62cb17fd39fc
Alex Korzh <alexkorzh7@gmail.com>
2021-07-07 23:41:16 +0200

    Implement sign in, and sign in and fetch user methods

--- Plasmatic/src/services/session.ts
+++ Plasmatic/src/services/session.ts
@@ -1,11 +1,14 @@
 import { injectable } from 'inversify';
 import { BehaviorSubject } from 'rxjs';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { UserAuthStatus } from '~/types/userAuthStatus';
 
 export interface ISessionService {
   userStatus: BehaviorSubject<UserAuthStatus>;
+  signInAndFetchUser: () => Promise<Error | null>;
+  signIn: () => Promise<[FirebaseAuthTypes.UserCredential | null, Error | null]>;
   stopUpdates: () => void;
 }
 
@@ -21,12 +24,35 @@ export class SessionService implements ISessionService {
   }
 
   constructor() {
+    GoogleSignin.configure({
+      webClientId: '',
+    });
     this._firebaseAuthStateChangeSubscriber = auth().onAuthStateChanged(this.onFirebaseAuthStateChanged);
   }
+
+  public signInAndFetchUser = async () => {
+    const [firebaseUserCredential, error] = await this.signIn();
+    if (firebaseUserCredential?.user) {
+      this._userStatus.next(UserAuthStatus.AUTHORIZED);
+    }
+    return error;
+  };
 
   public stopUpdates = () => {
     if (this._firebaseAuthStateChangeSubscriber) {
       this._firebaseAuthStateChangeSubscriber();
+    }
+  };
+
+  public signIn = async (): Promise<[FirebaseAuthTypes.UserCredential | null, Error | null]> => {
+    try {
+      const { idToken } = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      const firebaseUserCredential = await auth().signInWithCredential(googleCredential);
+      return [firebaseUserCredential, null];
+    } catch (e) {
+      console.log(e);
+      return [null, e];
     }
   };
 
