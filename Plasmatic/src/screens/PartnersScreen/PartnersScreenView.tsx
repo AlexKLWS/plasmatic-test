d2c6f18207021b9ae75a6da45d920dd347862c65
Alex Korzh <alexkorzh7@gmail.com>
2021-07-07 02:46:32 +0200

    Display parter category selectors

--- Plasmatic/src/screens/PartnersScreen/PartnersScreenView.tsx
+++ Plasmatic/src/screens/PartnersScreen/PartnersScreenView.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import PartnerCategoryItem from '~/components/partnerCategoryItem/PartnerCategoryItem';
 import PartnerOfferItem from '~/components/partnerOfferItem/PartnerOfferItem';
 import PartnersScreenHeader from '~/components/partnersScreenHeader/PartnersScreenHeader';
-import SmallEventItem from '~/components/smallEventItem/SmallEventItem';
 import { scale } from '~/helpers/scale';
 import styleSystem from '~/shared/styles';
 
@@ -17,11 +17,12 @@ const PartnersScreenView = () => {
             nestedScrollEnabled
             horizontal
             showsHorizontalScrollIndicator={false}
-            data={[1, 2, 3]}
+            data={['all', 'fashion', 'tech', 'travel', 'movies']}
             keyExtractor={i => `${i}`}
-            renderItem={() => <SmallEventItem />}
+            renderItem={({ item }) => <PartnerCategoryItem isSelected={false} category={item as any} />}
             contentContainerStyle={{ flexGrow: 1, paddingHorizontal: scale(16) }}
           />
+          <View style={{ height: scale(2), backgroundColor: styleSystem.colors.ui.homeBackground }} />
           <View style={styles.offersList}>
             <Text style={[styleSystem.typography.labelBig, { paddingBottom: scale(16) }]}>Top Offers</Text>
             <PartnerOfferItem />
