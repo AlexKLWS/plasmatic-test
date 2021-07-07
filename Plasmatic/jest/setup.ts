791e42b2916fb4cad143674ef0b38cd11dba813f
Alex Korzh <alexkorzh7@gmail.com>
2021-07-07 22:32:51 +0200

    Change jest setup to ts

--- Plasmatic/jest/setup.ts
+++ Plasmatic/jest/setup.ts
@@ -3,3 +3,5 @@ import 'react-native-gesture-handler/jestSetup';
 require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+import 'reflect-metadata';
