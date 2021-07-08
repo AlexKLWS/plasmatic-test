100da396947d9554363e2d9535b558a7a0cc4736
Alex Korzh <alexkorzh7@gmail.com>
2021-07-08 02:43:38 +0200

    Update and fix user spec

--- Plasmatic/src/services/user.spec.ts
+++ Plasmatic/src/services/user.spec.ts
@@ -6,20 +6,31 @@ import { IUserService, UserService, UserServiceId } from '~/services/user';
 import { ISessionService, SessionServiceId } from '~/services/session';
 import { UserAuthStatus } from '~/types/userAuthStatus';
 import { mockFirebaseUser } from './session.spec';
+import { IStorageService, StorageServiceId } from './storage';
+import { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 const mockUser = {
   displayName: 'Alex',
   email: 'alexkorzh7@gmail.com',
   photoURL: 'pic1',
+  location: 'Palm Springs, CA',
 };
 
 describe('fetchUserByEmail', () => {
   @injectable()
   class SessionServiceMock implements ISessionService {
+    firebaseUser: FirebaseAuthTypes.User | null = mockFirebaseUser as any;
     userStatus: BehaviorSubject<UserAuthStatus> = new BehaviorSubject<UserAuthStatus>(UserAuthStatus.AUTHORIZED);
     signIn = () => Promise.resolve([{ user: mockFirebaseUser }, null] as any);
     stopUpdates: () => void = () => undefined;
   }
+
+  @injectable()
+  class StorageServiceMock implements IStorageService {
+    storeObject: (key: string, value: any) => Promise<void> = () => Promise.resolve();
+    getObject: (key: string) => Promise<any> = () => Promise.resolve(null);
+  }
+
   it('Should successfully fetch user by email', async () => {
     // @ts-expect-error
     mockFirestore().collection.mockImplementationOnce((collectionName: string) => {
@@ -47,6 +58,7 @@ describe('fetchUserByEmail', () => {
     const myContainer = new Container();
     myContainer.bind<IUserService>(UserServiceId).to(UserService);
     myContainer.bind<ISessionService>(SessionServiceId).to(SessionServiceMock);
+    myContainer.bind<IStorageService>(StorageServiceId).to(StorageServiceMock);
 
     const instance = myContainer.get<IUserService>(UserServiceId);
     const [user, error] = await instance.fetchUserByEmail('alexkorzh7@gmail.com');
@@ -80,6 +92,7 @@ describe('fetchUserByEmail', () => {
     const myContainer = new Container();
     myContainer.bind<IUserService>(UserServiceId).to(UserService);
     myContainer.bind<ISessionService>(SessionServiceId).to(SessionServiceMock);
+    myContainer.bind<IStorageService>(StorageServiceId).to(StorageServiceMock);
 
     const instance = myContainer.get<IUserService>(UserServiceId);
     const [user, error] = await instance.fetchUserByEmail('alexkorzh7@gmail.com');
@@ -113,6 +126,7 @@ describe('fetchUserByEmail', () => {
     const myContainer = new Container();
     myContainer.bind<IUserService>(UserServiceId).to(UserService);
     myContainer.bind<ISessionService>(SessionServiceId).to(SessionServiceMock);
+    myContainer.bind<IStorageService>(StorageServiceId).to(StorageServiceMock);
 
     const instance = myContainer.get<IUserService>(UserServiceId);
     const [user, error] = await instance.fetchUserByEmail('alexkorzh7@gmail.com');
@@ -122,9 +136,16 @@ describe('fetchUserByEmail', () => {
 });
 
 describe('signInAndFetchUserByEmail', () => {
+  @injectable()
+  class StorageServiceMock implements IStorageService {
+    storeObject: (key: string, value: any) => Promise<void> = () => Promise.resolve();
+    getObject: (key: string) => Promise<any> = () => Promise.resolve(null);
+  }
+
   it('Should successfully sign in and fetch user by email', async () => {
     @injectable()
     class SessionServiceMock implements ISessionService {
+      firebaseUser: FirebaseAuthTypes.User | null = mockFirebaseUser as any;
       userStatus: BehaviorSubject<UserAuthStatus> = new BehaviorSubject<UserAuthStatus>(UserAuthStatus.AUTHORIZED);
       signIn = () => Promise.resolve([{ user: mockFirebaseUser }, null] as any);
       stopUpdates: () => void = () => undefined;
@@ -155,6 +176,7 @@ describe('signInAndFetchUserByEmail', () => {
     const myContainer = new Container();
     myContainer.bind<IUserService>(UserServiceId).to(UserService);
     myContainer.bind<ISessionService>(SessionServiceId).to(SessionServiceMock);
+    myContainer.bind<IStorageService>(StorageServiceId).to(StorageServiceMock);
 
     const instance = myContainer.get<IUserService>(UserServiceId);
     const error = await instance.signInAndFetchUserByEmail();
@@ -164,6 +186,7 @@ describe('signInAndFetchUserByEmail', () => {
   it('Should return error if sign in fails', async () => {
     @injectable()
     class SessionServiceMock implements ISessionService {
+      firebaseUser: FirebaseAuthTypes.User | null = mockFirebaseUser as any;
       userStatus: BehaviorSubject<UserAuthStatus> = new BehaviorSubject<UserAuthStatus>(UserAuthStatus.AUTHORIZED);
       signIn = () => Promise.resolve([null, new Error()] as any);
       stopUpdates: () => void = () => undefined;
@@ -194,6 +217,7 @@ describe('signInAndFetchUserByEmail', () => {
     const myContainer = new Container();
     myContainer.bind<IUserService>(UserServiceId).to(UserService);
     myContainer.bind<ISessionService>(SessionServiceId).to(SessionServiceMock);
+    myContainer.bind<IStorageService>(StorageServiceId).to(StorageServiceMock);
 
     const instance = myContainer.get<IUserService>(UserServiceId);
     const error = await instance.signInAndFetchUserByEmail();
