e0edda1c4e82381b905e5cc13a3293085f805d67
Alex Korzh <alexkorzh7@gmail.com>
2021-07-08 02:42:48 +0200

    Add async storage mock

--- Plasmatic/jest/setup.ts
+++ Plasmatic/jest/setup.ts
@@ -5,3 +5,8 @@ require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 import 'reflect-metadata';
+
+// @ts-expect-error
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
