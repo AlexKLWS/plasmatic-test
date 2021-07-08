62b98fb3af6160894047e78bfd4bae1203973a22
Alex Korzh <alexkorzh7@gmail.com>
2021-07-08 16:16:28 +0200

    Add storage service to container

--- Plasmatic/src/services/container.ts
+++ Plasmatic/src/services/container.ts
@@ -1,11 +1,13 @@
 import { Container, interfaces, ContainerModule } from 'inversify';
 
 import { ISessionService, SessionServiceId, SessionService } from './session';
+import { IStorageService, StorageService, StorageServiceId } from './storage';
 import { IUserService, UserService, UserServiceId } from './user';
 
 export const containerModule = new ContainerModule((bind: interfaces.Bind) => {
   bind<ISessionService>(SessionServiceId).to(SessionService).inSingletonScope();
   bind<IUserService>(UserServiceId).to(UserService).inSingletonScope();
+  bind<IStorageService>(StorageServiceId).to(StorageService).inSingletonScope();
 });
 
 export const container = new Container();
