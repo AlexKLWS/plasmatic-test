9e365f5743e48d1eadf9bae8edbd2c58423c04f0
Alex Korzh <alexkorzh7@gmail.com>
2021-07-08 21:15:29 +0200

    Clean up home stack imports

--- Plasmatic/src/navigation/HomeStack.tsx
+++ Plasmatic/src/navigation/HomeStack.tsx
@@ -5,13 +5,6 @@ import HomeScreen from '~/screens/HomeScreen';
 import ServicesScreen from '~/screens/ServicesScreen';
 import PartnersScreen from '~/screens/PartnersScreen';
 import ActivityScreen from '~/screens/ActivityScreen';
-import styleSystem from '~/shared/styles';
-import HomeIcon from '~/assets/icons/tabBar/HomeIcon';
-import ServicesIcon from '~/assets/icons/tabBar/ServicesIcon';
-import PartnersIcon from '~/assets/icons/tabBar/PartnersIcon';
-import ActivityIcon from '~/assets/icons/tabBar/ActivityIcon';
-import { View } from 'react-native';
-import { scale } from '~/helpers/scale';
 import HomeTabBar from '~/components/tabBar/homeTabBar';
 
 const Tab = createBottomTabNavigator();
