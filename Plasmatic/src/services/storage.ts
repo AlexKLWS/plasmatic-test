c497ce43d78ed44c1ca7cda7e00b3e7c11934ae4
Alex Korzh <alexkorzh7@gmail.com>
2021-07-08 02:43:26 +0200

    Add simple storage service

---
+++ Plasmatic/src/services/storage.ts
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export interface IStorageService {
+  storeObject: (key: string, value: any) => Promise<void>;
+  getObject: (key: string) => Promise<any | null>;
+}
+
+export class StorageService implements IStorageService {
+  public storeObject = async (key: string, value: any) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem(key, jsonValue);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  public getObject = async (key: string) => {
+    try {
+      const jsonValue = await AsyncStorage.getItem(key);
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+}
+
+export const StorageServiceId = Symbol('StorageService');
