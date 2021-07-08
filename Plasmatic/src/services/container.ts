f9834b61389c4ebf512968e13afe91c7d992d90d
Alex Korzh <alexkorzh7@gmail.com>
2021-07-08 22:33:10 +0200

    Add notification services to container

--- Plasmatic/src/services/container.ts
+++ Plasmatic/src/services/container.ts
@@ -1,4 +1,10 @@
 import { Container, interfaces, ContainerModule } from 'inversify';
+import {
+  INotificationAnalyticsService,
+  NotificationAnalyticsService,
+  NotificationAnalyticsServiceId,
+} from './analytics/notificationsAnalytics';
+import { INotificationService, NotificationService, NotificationServiceId } from './notifications';
 
 import { ISessionService, SessionServiceId, SessionService } from './session';
 import { IStorageService, StorageService, StorageServiceId } from './storage';
@@ -8,6 +14,10 @@ export const containerModule = new ContainerModule((bind: interfaces.Bind) => {
   bind<ISessionService>(SessionServiceId).to(SessionService).inSingletonScope();
   bind<IUserService>(UserServiceId).to(UserService).inSingletonScope();
   bind<IStorageService>(StorageServiceId).to(StorageService).inSingletonScope();
+  bind<INotificationService>(NotificationServiceId).to(NotificationService).inSingletonScope();
+
+  // Analytics
+  bind<INotificationAnalyticsService>(NotificationAnalyticsServiceId).to(NotificationAnalyticsService);
 });
 
 export const container = new Container();
