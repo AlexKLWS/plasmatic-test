d37c661f25a2900d86a5facb80438fcc903f3520
Alex Korzh <alexkorzh7@gmail.com>
2021-07-08 03:05:29 +0200

    Implement refresh user method, persist user data

--- Plasmatic/src/services/user.ts
+++ Plasmatic/src/services/user.ts
@@ -4,9 +4,12 @@ import { BehaviorSubject } from 'rxjs';
 
 import { User } from '~/types/user';
 import { ISessionService, SessionServiceId } from './session';
+import { IStorageService, StorageServiceId } from './storage';
+import { USER_STORAGE_KEY } from '~/consts/storageKeys';
 
 export interface IUserService {
   user: BehaviorSubject<User | null>;
+  refreshUser: () => Promise<Error | null>;
   fetchUserByEmail: (email: string) => Promise<[User | null, Error | null]>;
   signInAndFetchUserByEmail: () => Promise<Error | null>;
 }
@@ -14,6 +17,7 @@ export interface IUserService {
 @injectable()
 export class UserService implements IUserService {
   private readonly _sessionService: ISessionService;
+  private readonly _storageService: IStorageService;
 
   private readonly _user: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
 
@@ -21,8 +25,13 @@ export class UserService implements IUserService {
     return this._user;
   }
 
-  constructor(@inject(SessionServiceId) sessionService: ISessionService) {
+  constructor(
+    @inject(SessionServiceId) sessionService: ISessionService,
+    @inject(StorageServiceId) storageService: IStorageService,
+  ) {
+    this._storageService = storageService;
     this._sessionService = sessionService;
+    this.retrievePersistedUser();
   }
 
   public fetchUserByEmail = async (email: string): Promise<[User | null, Error | null]> => {
@@ -34,8 +43,18 @@ export class UserService implements IUserService {
       return [null, new Error('More than one user with the same email')];
     }
     const user = usersQuerySnapshot.docs[0].data() as User;
+    await this._storageService.storeObject(USER_STORAGE_KEY, user);
     this._user.next(user);
     return [user, null];
+  };
+
+  public refreshUser = async () => {
+    const email = this._sessionService.firebaseUser?.email;
+    if (email) {
+      const [_, userFetchError] = await this.fetchUserByEmail(email);
+      return userFetchError;
+    }
+    return new Error('No firebase user!');
   };
 
   public signInAndFetchUserByEmail = async () => {
@@ -46,6 +65,13 @@ export class UserService implements IUserService {
       return userFetchError;
     }
     return error;
+  };
+
+  private retrievePersistedUser = async () => {
+    const user = await this._storageService.getObject(USER_STORAGE_KEY);
+    if (user) {
+      this._user.next(user);
+    }
   };
 }
 
