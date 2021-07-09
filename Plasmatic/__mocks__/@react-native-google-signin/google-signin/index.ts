b9217780478b90af579a55f164bd98b8411d4b9a
Alex Korzh <alexkorzh7@gmail.com>
2021-07-09 21:05:15 +0200

    Mock google sign in revoke access and sign out methods

--- Plasmatic/__mocks__/@react-native-google-signin/google-signin/index.ts
+++ Plasmatic/__mocks__/@react-native-google-signin/google-signin/index.ts
@@ -2,5 +2,7 @@ const GoogleSignin: any = {};
 
 GoogleSignin.signIn = jest.fn(() => Promise.resolve({ idToken: 'GOOGLE_AUTH_TOKEN' }));
 GoogleSignin.configure = jest.fn();
+GoogleSignin.revokeAccess = jest.fn();
+GoogleSignin.signOut = jest.fn();
 
 export { GoogleSignin };
